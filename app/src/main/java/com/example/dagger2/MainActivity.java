0e6dfb889a03273273af43a2a85046ae495f9c55
mohamed <mohamed_gsm@msn.com>
2021-08-24 09:01:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dagger2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dagger2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.dagger2.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    public ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        
+    }
+}
