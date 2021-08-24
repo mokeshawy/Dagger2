d4e91fb81cfa0a5c1c85eb880f04be6ecb72b4c6
mohamed <mohamed_gsm@msn.com>
2021-08-24 13:38:38 +0200

    implement field inject in dagger

--- app/src/main/java/com/example/dagger2/CoffeeComponent.java
+++ app/src/main/java/com/example/dagger2/CoffeeComponent.java
@@ -4,5 +4,9 @@ import dagger.Component;
 
 @Component
 public interface CoffeeComponent {
+
     Coffee getCoffee();
+
+    // create this where we need using data member from activity or fragment with Dagger
+    void inject(MainActivity mainActivity);
 }
